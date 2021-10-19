299bdca8e85592dc54e4ff8e42fd39a611d8e3e2
Zayne Komichi <zaynekomichi@hotmail.com>
2021-10-19 16:15:53 +0200

    Added search function on tab 1 Inventory all functional and working

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -4,19 +4,24 @@ import { IonContent,
   IonTitle, 
   IonToolbar, 
   IonSearchbar,
+  IonButton,
   IonText,
+  IonInput,
   useIonViewWillEnter,
   IonList,
   IonItem,
   IonIcon,
   IonCard,
+  IonBadge,
   IonLabel,useIonAlert,IonAlert} from '@ionic/react';
-import {flaskSharp,calendar,cart,clipboard,storefront,qrCode} from 'ionicons/icons';
+import {flaskSharp,calendar,cart,clipboard,storefront,qrCode,search} from 'ionicons/icons';
 import {useState,useEffect} from 'react';
+import {useForm,Controller} from 'react-hook-form';
 import axios from 'axios';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [searchv, setSearch] = useState<string>("");
   const [showAlert,setAlert] = useState(false);
   const [name,setName] = useState<string>("");
   const [quantity,setQuantity] = useState<number>();
@@ -27,8 +32,13 @@ const Tab1: React.FC = () => {
   const [notes,setNotes] = useState<string>("");
   const [showInfo,setInfo] = useState(false);
   const [allData,setData] = useState<any>([]);
+  const [searchData,setSearchData] = useState<any>([]);
   const [viewError,setError] = useState<string>("");
-  const searchbar = document.querySelector('IonSearchbar');
+  const [viewSearchError,setSearchError] = useState<string>("");
+  const {register,handleSubmit,formState:{errors}} = useForm({
+    mode:"onTouched",
+    reValidateMode:"onChange"
+  });
   useIonViewWillEnter(() => {
     axios.get('http://192.168.1.23/App_Data/Inventory.php',{
       params:{
@@ -37,13 +47,35 @@ const Tab1: React.FC = () => {
     })
     .then((response:any)=>{
       setData(response.data);
-      console.log(allData)
       setError("");
     })
     .catch((error:any)=>{
       setError("Server is offline");
     });
   });
+
+  const onSubmit = (data:any) =>{
+    const userData:any = data.productName;
+    axios.get('http://192.168.1.23/App_Data/InventorySearch.php',{
+      params:{
+        getData:1,
+        name:userData,
+      },
+    })
+    .then((response:any)=>{
+      const data = response.data;
+      if(data.length>0){
+        setSearchData(data);
+      setSearchError("");
+      }else{
+         setSearchData(data);
+         setSearchError(`could not find ${searchv}`);
+      }
+    })
+    .catch((error:any)=>{
+      setError("Server is offline");
+    });
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -52,8 +84,40 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonSearchbar></IonSearchbar>
+        <form onSubmit={handleSubmit(onSubmit)} className="align_search">
+        <IonInput type="search" {...register("productName",{required:true})} onIonChange={e => setSearch(e.detail.value!)} name="productName" value={searchv} 
+        placeholder="search" className="top-search"/>
+        <IonButton type="submit"><IonIcon icon={search}/></IonButton>
+        </form>
+        {errors.productName && <IonBadge color="danger" className="general_padding">Product Name is required</IonBadge>}
         <div id="inv_main">
+        {viewSearchError}
+        <IonList>
+        {searchData.map((items:any)=>{
+          return(
+            <IonItem key={items.id} onClick={()=>{
+                    setName(items.productName);
+                    setCode(items.Code);
+                    setQuantity(items.Quantity);
+                    setProvider(items.Provider);
+                    setReceivedBy(items.ReceivedBy);
+                    setReceiveDate(items.ReceiveDate);
+                    setNotes(items.Notes);
+                    setAlert(true);
+
+                  }}>
+              <div className="ListItems">
+                      <img src="./assets/media/pills.png" className="List_img" alt="" />
+                      <IonLabel className="Label"><b>{items.productName}</b></IonLabel>
+                    </div>
+                      <div>
+                        <IonIcon icon={qrCode}/>
+                        <IonText className="textPadding">{items.Code}</IonText>
+                      </div> 
+            </IonItem>
+            )
+        })}
+        </IonList>
           <h3>In-stock</h3>
           <div className="row_cards">
           {viewError}
@@ -114,6 +178,8 @@ const Tab1: React.FC = () => {
           text:'OK',
           role:'redirect',
           handler: redirect=>{
+            setSearch("");
+            setSearchData([]);
             setAlert(false);
           }
         }
